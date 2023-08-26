b9c1b4de7faf41557605e3a50f3ff25f06b20a09
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-08-26 16:28:24 -0400

    Finished variable file

---
+++ Ultra-fundamentals/vars.py
@@ -0,0 +1,25 @@
+# name = value
+# variables declaration -> assign a variable name to a value
+# variable -> alias to a value
+# identifier = 23
+# print(identifier)
+# identifier = 20 # value re-assignment
+# print(identifier)
+
+# make a variable with your age
+# print it out to the screen
+# re-assign that to a different value
+# print that out to the screen
+# age = 90
+# print(age)
+# age = 89
+# print(age)
+
+# age = 90
+# print(age) # -> quotations marks: string
+
+# name = "Kirill"
+# print(name)
+
+# make a variable with a float value
+# print it out to the screen
