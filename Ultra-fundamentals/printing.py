aec3dba64ef8dd9dee8afc62e59b588f5d6576a1
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-08-26 16:00:36 -0400

    Finished printing file

---
+++ Ultra-fundamentals/printing.py
@@ -0,0 +1,13 @@
+# print(3.14)
+# In Python, words or text are wrapped in "", '' -> called strings
+# In Python, numbers are not wrapped in quotes -> integers
+# In Python, decimals are not wrapped in quotes either -> floats, or doubles, or longs
+# print("Hello, world")
+
+# Print your name
+# Print your age
+# Print a decimal
+print("Kirill")
+print()
+print(90)
+print(1.1)
