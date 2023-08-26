c119284b302b676b09fcba1fbdd1403db9e5fb1d
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-08-26 17:36:49 -0400

    Finished String section

---
+++ Fundamental-data-types/strings.py
@@ -0,0 +1,38 @@
+# Data types that we will go over
+# 1. strings
+# 2. booleans
+# 3. floats
+# 4. integers
+# 5. arrays/lists
+
+# string-> characters that are enclosed by "", ''
+
+# str.lower() where str is any string
+# strVar = "KIRILL"
+# strLower = strVar.lower()
+# # print(strLower)
+
+# # str.upper()
+# lowerVar = "kirill"
+# varUpper = lowerVar.upper()
+# print(varUpper)
+
+# str.replace(old, new)
+# str = "The cat ate a mouse"
+# replacedStr = str.replace("cat", "dog")
+# print(replacedStr)
+
+# str.strip()
+# str = "  kirill   ".strip()
+# print(str)
+
+# str = "kirill"
+# # kirill
+# # k[0], i[1]
+# print(str[-4])
+
+# string concatentation
+# adding strings together
+# str = "kirill"
+# newStr = str[0] + " something"
+# print(newStr)
