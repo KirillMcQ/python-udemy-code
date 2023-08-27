b6a222a9f504317d2278b58ef568baa7120a73ba
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-08-26 20:59:56 -0400

    Finsihed Array Methods file

---
+++ Fundamental-data-types/arrayMethods.py
@@ -0,0 +1,7 @@
+arr = [12, 3.145, True, "String"]
+# arr.pop() # No arguments provided, removes last element
+# arr.pop(idx)
+# arr.pop(1)
+arr.remove("String")
+print(arr)
+# arr.append(val) -> REMEMBER
