e324e48c851056f670fccbdbfff54a34f22950c7
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-08-26 20:42:16 -0400

    Finished array section

---
+++ Fundamental-data-types/arrays.py
@@ -0,0 +1,10 @@
+# Array/List -> store multiple values in a single variable
+arr = ["Kirill", " lastname", 3.14]
+sumBetweenTwo = arr[0] + arr[1]
+# 0-indexed
+# first element - index 0, second element - index 1, ...
+# print(len(arr))
+
+# How to access last element if unkown size of array
+testArr = [1, 2, 3, 4, 5]
+print(testArr[-1])
